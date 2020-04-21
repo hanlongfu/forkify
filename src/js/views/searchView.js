33c55151e953b73a47e0f5a4c33893a8db7afd49
hanlongfu <hfu@salemstate.edu>
2020-04-20 22:21:35 -0700

    pagination

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,6 +9,7 @@ export const clearInput = () => elements.searchInput.value = '';
 //clear previous results
 export const clearResults = () => {
   elements.searchResList.innerHTML = '';
+  elements.searchResPages.innerHTML = '';
 };
 
 //limit the recipe title to 17  words
@@ -48,6 +49,46 @@ const renderRecipe = recipe => {
   elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {
-  recipes.forEach(renderRecipe);
+//type: 'prev' or 'next'
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto = ${type === 'prev' ? page - 1 : page + 1} >
+        <svg class="search__icon">
+          <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1} </span>
+     </button>
+`;
+
+//render buttons
+const renderButton = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+  
+  let button;
+  if(page === 1 && pages > 1) {
+    //button to go to next page
+    button = createButton(page, 'next');
+  } else if( page < pages){
+    //Both buttons
+    button = `
+      ${createButton(page, 'prev')}
+      ${createButton(page, 'next')}
+    `;
+  } else if( page === pages && pages > 1){
+    // only button to go to prev page
+    button = createButton(page, 'prev');
+  }
+
+  elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+  //render results of current page
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage; 
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  //render pagination buttons
+  renderButton(page, recipes.length, resPerPage);
 };
